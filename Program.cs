d7a2ceb93f1a042bab64f8c6f81872002adc3191
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2022-11-24 21:18:25 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Домашка
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Переменную age получается поменять, а time нет, потому что age уже задействена 
+            int age = 18;
+            int time = 20;
+            Console.WriteLine(age);
+            Console.WriteLine(time);
+            int age2;
+            int time2;
+            age2 = Convert.ToInt32(time);
+            Console.WriteLine(age2);
+            time2 = Convert.ToInt32(age);
+            Console.WriteLine(time2);
+        }
+    }
+}
